f4302ae1b730bbf58a888e8c1b66ec875b54350f
Pratham <ppratham812@gmail.com>
2023-07-30 12:37:49 +0530

    Google sign-in developed

---
+++ backend/auth.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:5000/auth/google/callback",
+    passReqToCallback: true
+}, function(request, accessToken, refreshToken, profile, done) {
+    done(null, profile);
+}));
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+})
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+})
