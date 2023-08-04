7c0c720feaad2e6c440d0c57d11abfa4f7aabedb
Pratham <ppratham812@gmail.com>
2023-08-04 23:47:56 +0530

    feat:login functionality developed

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const passport = require('passport');
 const auth = require('./auth');
+const bcrypt = require('bcrypt');
 const session = require('express-session');
 const { connectDB } = require('./database/db');
 const PORT = process.env.PORT;
@@ -44,11 +45,20 @@ app.get('/', (req, res) => {
     res.send(`<h1>Backend is running</h1>`);
 })
 
-app.post('/api/login', (req, res) => {
-    const { email } = req.body;
-    const userInfo = { Email: email }
-    console.log(userInfo);
-    res.json({ msg: 'User data saved', userData: userInfo });
+app.post('/api/login', async(req, res) => {
+    const { email, password } = req.body;
+    const userInfo = { email, password };
+    const findUser = await User.findOne({ email });
+    if (!findUser) {
+        res.json({ success: false, error: 'No registered user found' });
+        return;
+    }
+    const passOk = await bcrypt.compare(findUser.password, userInfo.password);
+    if (!passOk) {
+        res.json({ success: false, error: 'Wrong E-mail OR password' });
+        return;
+    }
+    res.json({ success: true, msg: 'Password Correct' });
 })
 
 app.get('/auth/google', passport.authenticate('google', {
@@ -56,7 +66,7 @@ app.get('/auth/google', passport.authenticate('google', {
 }))
 
 app.get('/auth/google/callback', passport.authenticate('google', {
-    successRedirect: '/auth/success',
+    successRedirect: '/auth/success?email=:email',
     failureRedirect: '/auth/google/failure'
 }));
 
@@ -68,13 +78,29 @@ app.get('/auth/success', isLoggedin, async(req, res) => {
             email,
             name: displayName,
             imgURL: picture,
+            password: 'notset',
+            passwordSet: false,
         };
         const createdUser = await User.create(userData);
         console.log(createdUser);
-        res.redirect('http://localhost:3000/SuccessPage');
+        res.redirect(`http://localhost:3000/SuccessPage/${email}`);
     } catch (err) {
         console.log(err);
     }
+})
+
+app.post('/api/setPassword', async(req, res) => {
+    const { email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const getUser = await User.findOneAndUpdate({ email }, { "$set": { "password": hashedPassword, "passwordSet": true } }, { new: true });
+    console.log(getUser);
+    res.json({ getUser });
+})
+
+app.post('/api/checkPasswordStatus', async(req, res) => {
+    const { email } = req.body;
+    const getUser = await User.find({ email });
+    res.json(getUser);
 })
 
 app.get('/auth/google/failure', (req, res) => {
