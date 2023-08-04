0ade7f3d1ab009f32fda70eade9dd2c815054575
Pratham <ppratham812@gmail.com>
2023-08-04 23:47:34 +0530

    database schema modified

--- backend/database/User.js
+++ backend/database/User.js
@@ -3,6 +3,15 @@ const mongoose = require('mongoose');
 const User = new mongoose.Schema({
     email: {
         type: String,
+        unique: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    passwordSet: {
+        type: Boolean,
         required: true,
     },
     name: {
