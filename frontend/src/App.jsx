ec46de12ee93281c431949af59aa8004b7738537
Pratham <ppratham812@gmail.com>
2023-08-07 19:42:32 +0530

    fix:route not matching issue fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import FormComponent from './Components/FormComponent';
 import SuccessPage from './Components/SuccessPage';
 import MainPage from './Components/MainPage';
+const base_url = process.env.REACT_APP_FRONTEND_BASE_URI;
+
 function App() {
     return(
       <Router>
         <Routes>
-          <Route path="/" element={<FormComponent/>} />
-          <Route path="/SuccessPage/:email" element={<SuccessPage/>} />
-          <Route path="/main" element={<MainPage/>}/>
+          <Route path={`${base_url}`} element={<FormComponent/>} />
+          <Route path={`${base_url}/SuccessPage/:email`} element={<SuccessPage/>} />
+          <Route path={`${base_url}/main`} element={<MainPage/>}/>
         </Routes>
       </Router> 
     )
