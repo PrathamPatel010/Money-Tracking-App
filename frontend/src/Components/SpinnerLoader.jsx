4fe5c039e5258883497c17ec458d54f395243fac
Pratham <ppratham812@gmail.com>
2023-10-31 14:23:01 +0530

    Loader added in login page

---
+++ frontend/src/Components/SpinnerLoader.jsx
@@ -0,0 +1,13 @@
+import { InfinitySpin } from 'react-loader-spinner';
+const SpinnerLoader = () => {
+    return (
+        <div>
+            <InfinitySpin
+                width='200'
+                color="#42F7F1"
+            />
+        </div>
+    )
+}
+
+export default SpinnerLoader;
